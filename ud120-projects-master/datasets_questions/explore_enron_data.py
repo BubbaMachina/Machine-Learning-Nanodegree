a1938d8ca169c12a339a3366d48df8ee2be3293a
SimplifyData <atif.zafar@sds.technology>
2016-08-23 17:14:46 -0700

    enron data set - df created after testing the algorithm for hours

--- ud120-projects-master/datasets_questions/explore_enron_data.py
+++ ud120-projects-master/datasets_questions/explore_enron_data.py
@@ -16,6 +16,8 @@
 """
 import numpy as np
 
+import pandas as pd
+
 import pickle
 
 import re
@@ -153,10 +155,147 @@ print "LAY KENNETH L"
 
 print type(enron_data['METTS MARK']['deferral_payments'])
 
+print "\n creating a data frame of enron data \n"
+
+print "\n enron data \n"
+
 print enron_data
 
+print "\n keys of enron data \n"
+
+print keys
+
+enron_df_columns_list = list()
+
+enron_df_columns_list.append('name')
+
+for col in enron_data[keys[0]].keys():
+    enron_df_columns_list.append(col)
+
+print "\n columns created for enron data df \n"
+
+enron_df_columns = ','.join(enron_df_columns_list)
+
+print enron_df_columns
+
+
+"""
+#enron_data[keys[0]]["name"] = keys[0]
+
+#enron_data_df = pd.DataFrame(columns= enron_df_columns_list)
+
+#enron_data_df["name"] = keys
+
+#print enron_data_df.head()
+
+#print "\n empty enron data df \n"
+
+#print enron_data_df
+
+#print "\n appending the enron data into the df \n"
+
+#print enron_data.keys()[0]
+
+#print enron_data[keys[0]]
+
+#enron_data[keys[0]]['name'] = keys[0]
+
+"""
+
+print '\n Creating enron data list\n '
+
+print (enron_data[keys[0]])
+
+enron_data_list = list()
+#sub_key_list = list()
+
+for key in keys:
+    #print key
+
+    sub_key_list = list()
+
+    enron_data[key]["name"] = key
+
+    for sub_key in enron_df_columns_list:
+        #print sub_key
+
+        #print enron_data[key][sub_key]
+
+        sub_key_list.append(enron_data[key][sub_key])
+
+    enron_data_list.append(sub_key_list)
+
+        #print sub_key_list
+print enron_data_list
+
+"""
+
+#enron_data[keys[1]]["name"] = keys[1]
+
+#print pd.Series(enron_data[keys[0]])
+
+#enron_data_df.loc[0] = pd.Series(enron_data[keys[0]])
+
+#enron_data_df.append(pd.Series(enron_data[keys[1]]),ignore_index= True)
+
+#print "\n appending the df\n"
+
+#print enron_data_df.head()
+'''
+for name in enron_data:
+    #print name
+    #print enron_data[name]
+
+    enron_data[name]['name'] = name
+
+    #print enron_data[name].keys()
+
+    #print enron_data[name].values()
 
 
 
+    df = pd.DataFrame([enron_data[name]])
+
+    enron_data_df.append(df)
+
+    #print df
+    #enron_data_df.append(df)
+
+    #enron_data_df
 
 
+
+    #rint df
+
+
+    #print df
+
+    #print df.shape[0]
+
+    #enron_data_df.loc[enron_data_df["name"] == name] = df.values
+
+    #enron_data_df.append(df)
+
+    #print enron_data_df
+
+print "done \n"
+
+print enron_data_df.head()
+
+#print enron_data_df
+
+print df.head()
+
+#enron_data_df = pd.DataFrame(enron_data[])
+
+#print enron_data_df.head()
+
+'''
+"""
+
+
+print "\n creating a df \n"
+
+enron_data_df = pd.DataFrame(enron_data_list, columns=enron_df_columns_list)
+
+print enron_data_df.head()
