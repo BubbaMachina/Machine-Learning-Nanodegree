8b29fbb49a3f9e6091f71d0e2fde01cedde8e306
Simplify Data <atif.zafar@sds.technology>
2016-09-12 20:02:39 -0700

    Neural Networks - XOR Perceptron - seperating inputvalues weights for activation

--- Neural Networks/XOR Perceptron test.py
+++ Neural Networks/XOR Perceptron test.py
@@ -56,7 +56,11 @@ class Perceptron:
             return [np.sum(strength), 0]
 
     def single_node_activation(self, inputvalues, layer):
-        print "single node activation"
+        print "\n^^^^^^^^Entering single node activation^^^^^^^^^^^^^^^\n"
+
+        print "\n^^^^^^^^^^^^Exiting single node activation^^^^^^^^^^^^^^^\n"
+
+        return None
 
     def multiple_node_activation(self, inputvalues, layer):
         """
@@ -67,6 +71,8 @@ class Perceptron:
         :return:
         """
 
+        print "\n *********Entering multiple nodes activation *********** \n"
+
         # checking the number of nodes in a layer
         print layer
 
@@ -74,9 +80,59 @@ class Perceptron:
 
         for nodes in layer:
             if type(nodes) is not list:
+                print "\n**********Exiting multiple nodes activation**********\n "
+
                 return self.single_node_activation(inputvalues,layer)
 
             else:
+
+                print "\n---------------------number of items in the input parameters------------------ "
+
+                print "\n inputs: ", inputvalues
+
+                print "\n layer: ", layer
+
+                print "\n nodes: ", nodes
+
+                input_counts = len(inputvalues)
+
+                print "\n number of inputs: ", input_counts
+
+                layers_count = len(layer)
+
+                print "\n number of layers: ", layers_count
+
+                nodes_count = len(layer[0])
+
+                print "\n nodes in each layer: ", nodes_count
+
+                print "\n -----------------End of number of items in the argument--------------------"
+
+                break
+
+        print "\n -----------------Seperating weight nodes for computation-----------------------\n"
+
+        weights_list = list()
+
+        for node_count in range(nodes_count):
+            nodes_list = list()
+
+            for nodes in layer:
+                #print nodes[node_count]
+
+                nodes_list.append(nodes[node_count])
+
+            print "\n weights of the nodes: ", nodes_list
+
+            weights_list.append(nodes_list)
+
+        print "\n weights in the layer: ", weights_list
+
+        weights_list_count = len(weights_list)
+
+        print "\n number of weight nodes in the layer: ", weights_list_count
+
+        print "\n ----------------End of seperation of weight nodes for activation ------------------\n"
 
 
 
